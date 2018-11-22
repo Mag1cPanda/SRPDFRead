1a9b019bd0ba6c2e02b2921194e4aee5c55c09c1
潘神 <415391360@qq.com>
2018-11-22 16:27:09 +0800

    Initial Commit

---
+++ SRPDFReaderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SRPDFReaderDemo
+//
+//  Created by longrise on 2018/11/22.
+//  Copyright © 2018 longrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
